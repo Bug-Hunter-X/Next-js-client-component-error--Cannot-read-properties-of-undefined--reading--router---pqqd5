7cad9679596223febc6803a5ffb75d74cdc0c563
BugHunter-X <askzenai@gmail.com>
2025-01-18 22:10:11 +0100

    Add about.js file

---
+++ about.js
@@ -0,0 +1,24 @@
+```javascript
+// pages/index.js
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to my Next.js app</h1>
+      <Link href='/about'>
+        <a>Go to About Page</a>
+      </Link>
+    </div>
+  );
+}
+```
+```javascript
+// pages/about.js
+function About() {
+  // This will cause an error because 'useRouter' is not available in the client component
+  const router = useRouter();
+  return <div>About Page</div>;
+}
+export default About;
+```
